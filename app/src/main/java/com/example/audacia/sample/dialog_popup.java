14ebead63e7fb7fff57307c8efca9b89226fddc8
The-Audacia <the.audacia@gmail.com>
2016-10-19 18:13:16 +0900

    backup

---
+++ app/src/main/java/com/example/audacia/sample/dialog_popup.java
@@ -0,0 +1,56 @@
+package com.example.audacia.sample;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View.OnTouchListener;
+import android.content.Context;
+import android.view.*;
+import android.widget.*;
+
+
+public class dialog_popup extends Dialog implements OnTouchListener {
+    private EditText travelName;
+    private Button addOK, addCancel;
+    private String _travelName;
+
+    public dialog_popup(Context context){
+        super(context);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_popup);
+
+        travelName = (EditText)findViewById(R.id.travelName);
+        addOK = (Button)findViewById(R.id.addOK);
+        addCancel = (Button)findViewById(R.id.addCancel);
+
+        addOK.setOnTouchListener(this);
+        addCancel.setOnTouchListener(this);
+    }
+
+    public String getTravelName(){
+        return _travelName;
+    }
+
+    public boolean onTouch(View v, MotionEvent event){
+        if(v == addOK){
+            _travelName = travelName.getText().toString();
+            dismiss();
+        }
+        else if(v == addCancel)
+            cancel();
+
+        return false;
+    }
+
+}
+
+
+
+
+
+
+
+
